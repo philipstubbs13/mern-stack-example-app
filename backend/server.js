b9db0507c4d676069a946360e0c069f328b2d99f
Phil Stubbs <philipstubbs13@gmail.com>
2024-07-27 15:54:00 -0500

    mern stack exaample app

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 const port = process.env.PORT || 5001;
 import userRoutes from "./routes/userRoutes.js";
+import path from "path";
 
 connectDB();
 
@@ -13,9 +15,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cookieParser());
+
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => res.send("Server is ready"));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Server is ready"));
+}
 
 app.use(notFound);
 app.use(errorHandler);
